90bf9ad1e652c69258040dc74be304afe3d30a3a
duyphuong403 <39291007+duyphuong403@users.noreply.github.com>
2019-04-12 21:20:45 +0700

    Update login.php

--- login.php
+++ login.php
@@ -88,9 +88,27 @@ if (isset($_POST['login'])) {
     <br><br>
 </div>
 </body>
+<footer class="page-footer font-medium blue fixed-bottom" style="padding: 0">
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3" style="color: white">
+        © 2018 Copyright: Quang Thien - Design and Developed by PN Team.
+    </div>
+    <!-- Copyright -->
+</footer>
+<!--/.Footer-->
+<script type="text/javascript" src="<?php echo PATH; ?>/public/lib/reactjs/react.production.min.js"></script>
+<script type="text/javascript" src="<?php echo PATH; ?>/public/lib/reactjs/react-dom.production.min.js"></script>
+<script type="text/javascript" src="<?php echo PATH; ?>/public/lib/reactjs/babel.min.js"></script>
 
-<?php include "layout/footer.php";
+<script type="text/javascript" src="<?php echo PATH; ?>/public/lib/mdb/js/jquery-3.3.1.min.js"></script>
+<script src="<?php echo PATH; ?>/public/lib/mdb/js/popper.min.js"></script>
+<script src="<?php echo PATH; ?>/public/lib/mdb/js/bootstrap.min.js"></script>
+<script src="<?php echo PATH; ?>/public/mdb/js/mdb.min.js"></script
+<script type="text/babel" src="<?php echo PATH; ?>/public/js/main.js"></script>
+<script type="text/javascript" src="<?php echo PATH; ?>/public/lib/sweetalert.min.js"></script>
+<script type="text/javascript" src="<?php echo PATH; ?>/public/js/javascript.js"></script>
 
-?>
+</body>
+</html>
 
 
